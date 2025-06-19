53a9d799c9c6f7995131d9d9b895612296d3d898
kanadkapil <118840231+kanadkapil@users.noreply.github.com>
2025-06-19 23:16:38 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import BlogDetails from './pages/BlogDetails';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import NotFound from './pages/NotFound';
-import CategoryPage from './pages/CategoryPage';
-import AuthorsList from './pages/AuthorsList';
-import AuthorProfile from './pages/AuthorProfile';
-import AuthorPostsPage from './pages/AuthorPostPage';
+// src/App.jsx
+import Layout from './pages/Layout';
 
 function App() {
-    return (
-        <div
-            className="min-h-screen bg-cover bg-center bg-no-repeat bg-black"
-            style={{
-                backgroundImage:
-                    "url('https://images.pexels.com/photos/2387793/pexels-photo-2387793.jpeg')",
-            }}
-        >
-            <Router>
-                <Navbar />
-
-                <div className="max-w-7xl mx-auto px-4 py-6 my-5 backdrop-blur-sm min-h-[80vh] rounded-xl shadow-lg bg-black/30">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/blog/:id" element={<BlogDetails />} />
-                        <Route path="/author/:authorID" element={<AuthorProfile />} />
-                        <Route path="/author/:authorID/posts" element={<AuthorPostsPage />} />
-                        <Route path="/authors" element={<AuthorsList />} />
-                        <Route path="/category/:tagName" element={<CategoryPage />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
-                </div>
-
-                <Footer />
-            </Router>
-        </div>
-    );
+    return <Layout />;
 }
 
 export default App;
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import BlogDetails from './BlogDetails';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import NotFound from './NotFound';
+import CategoryPage from './CategoryPage';
+import AuthorsList from './AuthorsList';
+import AuthorProfile from './AuthorProfile';
+import AuthorPostsPage from './AuthorPostPage';
+
+function App() {
+    return (
+        <div
+            className="min-h-screen bg-cover bg-center bg-no-repeat bg-black"
+            style={{
+                backgroundImage:
+                    "url('https://images.pexels.com/photos/2387793/pexels-photo-2387793.jpeg')",
+            }}
+        >
+            <Router>
+                <Navbar />
+
+                <div className="max-w-7xl mx-auto px-4 py-6 my-5 backdrop-blur-sm min-h-[80vh] rounded-xl shadow-lg bg-black/30">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/blog/:id" element={<BlogDetails />} />
+                        <Route path="/author/:authorID" element={<AuthorProfile />} />
+                        <Route path="/author/:authorID/posts" element={<AuthorPostsPage />} />
+                        <Route path="/authors" element={<AuthorsList />} />
+                        <Route path="/category/:tagName" element={<CategoryPage />} />
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
+                </div>
+
+                <Footer />
+            </Router>
+        </div>
+    );
+}
+
+export default App;
